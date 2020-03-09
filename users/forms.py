95abfe29efd692be60bbcf43b6838b91267ca596
unknown <sheampho@gmail.com>
2020-03-09 09:31:01 -0700

    Updated user files

--- users/forms.py
+++ users/forms.py
@@ -3,31 +3,24 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
+
 class UserRegisterForm(UserCreationForm):
-    #email = forms.EmailField
-    #constituancy = forms.CharField(max_length=200)
-    #last_name = forms.CharField
+    email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email','first_name','last_name','password1', 'password2']
-class RegisterForm(forms.ModelForm):
+        fields = ['username', 'email', 'password1', 'password2']
 
-    class Meta:
-        model = Profile
-        fields = ['image']
 
 class UserUpdateForm(forms.ModelForm):
-   # email = forms.EmailField
-
-    # FirstName = forms.CharField(max_length=50)
-    # LastName = forms.CharField(max_length=50)
+    email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email']
 
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields =['image']
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,39 +1,42 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm, RegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        i_form = RegisterForm(request.POST)
-        if form.is_valid() and i_form.is_valid():
+        if form.is_valid():
             form.save()
-            i_form.save()
-            messages.success(request, f'Account has been created! you can now login')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
-
     else:
         form = UserRegisterForm()
-        i_form = RegisterForm()
-    return render(request, 'users/register.html', {'form': form},{'i_form':i_form})
+    return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'your account has been successfully updated')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-    context ={
+
+    context = {
         'u_form': u_form,
         'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)
