931b4f2a7b9652b003e3292f48ed093ae1efb1c6
unknown <sheampho@gmail.com>
2020-03-09 05:45:06 -0700

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,18 +130,15 @@ MEDIA_URL ='/media/'
 # Extra lookup directories for collectstatic to find static files
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-#  Add configuration for static files storage using whitenoise
-
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
