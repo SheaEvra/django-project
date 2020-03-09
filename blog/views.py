5a75f9164d75661c808b79a7bd81795c6536b837
unknown <sheampho@gmail.com>
2020-03-09 10:17:21 -0700

    Updated blog views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.contrib import messages
@@ -18,6 +19,9 @@ class PostListView(ListView):
     ordering = ['-date_posted']
     paginate_by = 5
     model = Post
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
