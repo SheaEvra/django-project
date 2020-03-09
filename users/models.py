a6ff6259a602c3c855b89b49130edb9d581db44f
unknown <sheampho@gmail.com>
2020-03-09 09:39:28 -0700

    Updated users models

--- users/models.py
+++ users/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from  PIL import Image
+from PIL import Image
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{ self.user} profile'
-
-    def save(self):
-        super().save()
-
-        #img = Image.open(self.image.path)
-        #if img.height > 200 or img.width > 200:
-           # output_zise =(200, 200)
-            #img.thumbnail(output_zise)
-            #img.save(self.image.path)
+        return f'{self.user.username} Profile'
